4e35d13c802138ee70fa0dc132fc172eff18694a
Judah <judahtravis@gmail.com>
2016-04-02 14:20:50 -0700

    Added files via upload

---
+++ judah-final (2).py
@@ -0,0 +1,37 @@
+import time
+print "\n"
+print "\n"
+print "You are part of an elite team of students-- a member of Team: Blue Cheetahs."
+raw_input("Press Enter to continue...")
+print "\n"
+print "As a group, you travel to the planet Pythonica to learn the ways of the Pythonistas."
+raw_input("Press Enter to continue...")
+print "\n"
+print "In a dramatic twist, you are separated from members of your team."
+raw_input("Press Enter to continue...")
+print "\n"
+print "You are confused, hungry, and alone. Radio contact is limited and your battery is low." 
+print "\n"
+time.sleep(2)
+print "Time is running out!"
+raw_input("Press Enter to continue...")
+print "\n"
+print "To learn Python and survive, you must follow the path of the ancients:"
+time.sleep(2)
+print "observation, testing, reproduction, reasoning, and logic."
+time.sleep(3)
+print "\n"
+print "If you follow the path, the Pythonistas will reward you with knowledge."
+print "\n"
+time.sleep(3)
+print "To begin, please enter your name:"
+raw_input("Type your name to continue:")
+print "\n"
+print "WRONG!"
+print "\n"
+print "You are so disapointing. You'll never be any good at this. Stick with the liberal arts."
+print "\n"
+print "And would it kill you to call your mother? She misses you."
+print "\n"
+print "My ~real~ code fell apart about two hours ago. Eff."
+
